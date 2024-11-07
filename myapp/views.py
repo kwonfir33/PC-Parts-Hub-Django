11d4e2356d2cd384791ca5d6f1fab4478d8883ce
alecgerald <alecgerald.ricana@gmail.com>
2024-11-07 14:50:27 +0800

    Added CSRF token and functioning add to cart

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
+import json
 # handles request/response logic view > model > template
 
 def home(request):
@@ -24,37 +26,79 @@ def feedback(request):
     context = {}
     return render(request, 'feedback.html', context)
 
-
-
 def products(request):
-    products = Product.objects.all()
-    context = {'products':products}
+    context = {}
     return render(request, 'products.html', context)
 
-def cart(request):
-    if request.user.is_authenticated:
-        #user logged in (saved items rendered)
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-    else:
-        #user not logged in(set all values to 0) 
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+# def products(request):
 
-    context = {'items': items, 'order': order}
-    return render(request, 'cart.html', context)
+# 	if request.user.is_authenticated:
+# 		customer = request.user.customer
+# 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+# 		items = order.orderitem_set.all()
+# 		cartItems = order.get_cart_items
+# 	else:
+# 		#Create empty cart for now for non-logged in user
+# 		items = []
+# 		order = {'get_cart_total':0, 'get_cart_items':0}
+# 		cartItems = order['get_cart_items']
 
-def checkout(request):
-    if request.user.is_authenticated:
-        #user logged in (saved items rendered)
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-    else:
-        #user not logged in(set all values to 0) 
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+# 	products = Product.objects.all()
+# 	context = {'products':products, 'cartItems':cartItems}
+# 	return render(request, 'products.html', context)
 
-    context = {'items': items, 'order': order}
-    return render(request, 'checkout.html', context)
+# def cart(request):
+
+# 	if request.user.is_authenticated:
+# 		customer = request.user.customer
+# 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+# 		items = order.orderitem_set.all()
+# 		cartItems = order.get_cart_items
+# 	else:
+# 		#Create empty cart for now for non-logged in user
+# 		items = []
+# 		order = {'get_cart_total':0, 'get_cart_items':0}
+# 		cartItems = order['get_cart_items']
+
+# 	context = {'items':items, 'order':order, 'cartItems':cartItems}
+# 	return render(request, 'cart.html', context)
+
+# def checkout(request):
+# 	if request.user.is_authenticated:
+# 		customer = request.user.customer
+# 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+# 		items = order.orderitem_set.all()
+# 		cartItems = order.get_cart_items
+# 	else:
+# 		#Create empty cart for now for non-logged in user
+# 		items = []
+# 		order = {'get_cart_total':0, 'get_cart_items':0}
+# 		cartItems = order['get_cart_items']
+
+# 	context = {'items':items, 'order':order, 'cartItems':cartItems}
+# 	return render(request, 'checkout.html', context)
+
+# def updateItem(request):
+# 	data = json.loads(request.body)
+# 	productId = data['productId']
+# 	action = data['action']
+# 	print('Action:', action)
+# 	print('Product:', productId)
+
+# 	customer = request.user.customer
+# 	product = Product.objects.get(id=productId)
+# 	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+# 	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+# 	if action == 'add':
+# 		orderItem.quantity = (orderItem.quantity + 1)
+# 	elif action == 'remove':
+# 		orderItem.quantity = (orderItem.quantity - 1)
+
+# 	orderItem.save()
+
+# 	if orderItem.quantity <= 0:
+# 		orderItem.delete()
+
+# 	return JsonResponse('Item was added', safe=False)
