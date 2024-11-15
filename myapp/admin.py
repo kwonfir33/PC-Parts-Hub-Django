b423b2f0587e70c33af74d649a5d131cf2fd0431
alecgerald <alecgerald.ricana@gmail.com>
2024-11-15 22:05:05 +0800

    b r o k e n

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-# admin.site.register(Customer)
-# admin.site.register(Product)
-# admin.site.register(Order)
-# admin.site.register(OrderItem)
-# admin.site.register(ShippingAddress)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
 
 
 # super acc
 # Username: alec
 # Email Address: alec@gmail.com
 # Password: alectest
-
-
